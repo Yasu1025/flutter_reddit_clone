ad1ac96b0c27ef989f7e50efbd4eaa6856b0f85f
Yasu1025 <sillyjp25@gmail.com>
2023-01-18 09:50:12 +0900

    add loginscreen and assets

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const logoImgPath = 'assets/images/logo.png';
+  static const loginEmoteImgPath = 'assets/images/loginEmote.png';
+  static const googleIconImgPath = 'assets/images/google.png';
+}
