50b22f4a90683cfaa919934cc6acf4068c4cc608
Yasu1025 <sillyjp25@gmail.com>
2023-02-01 09:14:53 +0900

    add home screen and Providers for user

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/utils.dart';
@@ -13,11 +14,23 @@ final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   ),
 );
 
+final authStateChangeProvider = StreamProvider<User?>((ref) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.authStateChange;
+});
+
+final getUserProvider = StreamProvider.family((ref, String uid) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.getUserById(uid);
+});
+
 class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
   final Ref _ref;
   // super -> state is for loading
   AuthController(this._authRepository, this._ref) : super(false);
+
+  Stream<User?> get authStateChange => _authRepository.authStateChange;
 
   void signInWithGoogle(BuildContext ctx) async {
     state = true;
@@ -29,5 +42,9 @@ class AuthController extends StateNotifier<bool> {
       (userModel) =>
           _ref.read(userProvider.notifier).update((state) => userModel),
     );
+  }
+
+  Stream<UserModel> getUserById(String uid) {
+    return _authRepository.getUserById(uid);
   }
 }
