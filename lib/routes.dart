bc6f55191af402bc2e1049644227f2b26701b70f
Yasu1025 <sillyjp25@gmail.com>
2023-02-05 01:41:26 +0900

    add drawer and create_community_screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,7 +3,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:reddit_clone/features/auth/screens/login.dart';
-import 'package:reddit_clone/features/home/home_screen.dart';
+import 'package:reddit_clone/features/community/screens/create_community_screen.dart';
+import 'package:reddit_clone/features/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(routes: {
@@ -12,4 +13,6 @@ final loggedOutRoute = RouteMap(routes: {
 
 final loggedInRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create_community': (_) =>
+      const MaterialPage(child: CreateCommunityScreen()),
 });
