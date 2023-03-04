8b66fd7fdddd11c4e0622677825c7a7b6cf599be
Yasu1025 <sillyjp25@gmail.com>
2023-03-04 11:16:43 +0900

    add userProfile screen

--- lib/features/home/drawers/profile_drawer.dart
+++ lib/features/home/drawers/profile_drawer.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/theme/pallete.dart';
+import 'package:routemaster/routemaster.dart';
 
 class ProfileDrawer extends ConsumerWidget {
   const ProfileDrawer({super.key});
+
+  void navigateToUserProfile(BuildContext ctx, String uid) {
+    Routemaster.of(ctx).push('/user/$uid');
+  }
 
   void onLogout(WidgetRef ref) {
     ref.read(authControllerProvider.notifier).logout();
@@ -15,43 +20,44 @@ class ProfileDrawer extends ConsumerWidget {
     final user = ref.watch(userProvider)!;
 
     return Drawer(
-        child: SafeArea(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          CircleAvatar(
-            backgroundImage: NetworkImage(user.profileImg),
-            radius: 70,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text(
-            user.name,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
+      child: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          const Divider(),
-          ListTile(
-            title: const Text('My Profile'),
-            leading: const Icon(Icons.person),
-            onTap: () {},
-          ),
-          ListTile(
-            title: const Text('Logout'),
-            leading: Icon(
-              Icons.logout,
-              color: Pallete.redColor,
+            CircleAvatar(
+              backgroundImage: NetworkImage(user.profileImg),
+              radius: 70,
             ),
-            onTap: () => onLogout(ref),
-          ),
-          Switch.adaptive(value: true, onChanged: (val) {})
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              user.name,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              title: const Text('My Profile'),
+              leading: const Icon(Icons.person),
+              onTap: () => navigateToUserProfile(context, user.uid),
+            ),
+            ListTile(
+              title: const Text('Logout'),
+              leading: Icon(
+                Icons.logout,
+                color: Pallete.redColor,
+              ),
+              onTap: () => onLogout(ref),
+            ),
+            Switch.adaptive(value: true, onChanged: (val) {})
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -9,6 +9,7 @@ import 'package:reddit_clone/features/community/screens/create_community_screen.
 import 'package:reddit_clone/features/community/screens/edit_community_screen.dart';
 import 'package:reddit_clone/features/community/screens/mod_tools_screen.dart';
 import 'package:reddit_clone/features/home/screens/home_screen.dart';
+import 'package:reddit_clone/features/user_profile/screens/user_profile_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(routes: {
@@ -20,18 +21,28 @@ final loggedInRoute = RouteMap(routes: {
   '/create_community': (_) =>
       const MaterialPage(child: CreateCommunityScreen()),
   '/community/:name': (route) => MaterialPage(
-        child: CommunityScreen(communityID: route.pathParameters['name']!),
+        child: CommunityScreen(
+          communityID: route.pathParameters['name']!,
+        ),
       ),
   '/mod-tools/:id': (route) => MaterialPage(
-          child: ModToolsScreen(
-        communityID: route.pathParameters['id']!,
-      )),
+        child: ModToolsScreen(
+          communityID: route.pathParameters['id']!,
+        ),
+      ),
   '/edit-community/:id': (route) => MaterialPage(
-          child: EditCommunityScreen(
-        communityID: route.pathParameters['id']!,
-      )),
+        child: EditCommunityScreen(
+          communityID: route.pathParameters['id']!,
+        ),
+      ),
   '/add-mods/:id': (route) => MaterialPage(
-          child: AddModsScreen(
-        communityID: route.pathParameters['id']!,
-      )),
+        child: AddModsScreen(
+          communityID: route.pathParameters['id']!,
+        ),
+      ),
+  '/user/:uid': (route) => MaterialPage(
+        child: UserProfileScreen(
+          uid: route.pathParameters['uid']!,
+        ),
+      ),
 });
